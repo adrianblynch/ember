26da0bae78bd5dd50ac0ff18e36fcb0d632d6895
Adrian Lynch <adi_ady_ade@hotmail.com>
2014-07-21 09:14:32 +0100

    Fixed item class binding

--- todo/js/router.js
+++ todo/js/router.js
@@ -1,3 +1,9 @@
+Todos.TodosRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.find("todo");
+	}
+});
+
 Todos.Router.map(function() {
 	this.resource("todos", {path: "/"});
 });
