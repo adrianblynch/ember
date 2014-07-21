33d2fbccb95bfd8cd3529304f2ce127368f6bb83
Adrian Lynch <adi_ady_ade@hotmail.com>
2014-07-21 10:37:18 +0100

    Remove todo items

---
+++ todo/js/controllers/todo_controller.js
@@ -0,0 +1,26 @@
+Todos.TodoController = Ember.ObjectController.extend({
+	actions: {
+		editTodo: function() {
+			this.set("isEditing", true);
+		},
+		removeTodo: function() {
+			var todo = this.get('model');
+			todo.deleteRecord();
+			todo.save();
+		}
+	},
+	isEditing: false,
+	isCompleted: function(key, value) {
+
+		var model = this.get("model");
+
+		if (value === undefined) {
+			return model.get("isCompleted");
+		} else {
+			model.set("isCompleted", value);
+			model.save();
+			return value;
+		}
+
+	}.property("model.isCompleted")
+});
--- todo/js/controllers/todos_controller.js
+++ todo/js/controllers/todos_controller.js
@@ -23,6 +23,26 @@ Todos.TodosController = Ember.ArrayController.extend({
 
 			this.set("newTitle", "");
 
+		},
+		acceptChanges: function() {
+			this.set("isEditing", false);
+			if (Ember.isEmpty(this.get("model.title"))) {
+				this.send("removeTodo");
+			} else {
+				this.get("model").save();
+			}
+		},
+		removeTodo: function() {
+			var todo = this.get("model");
+			todo.deleteRecord();
+			todo.save();
 		}
-	}
+	},
+	remaining: function() {
+		return this.filterBy("isCompleted", false).get("length");
+	}.property("@each.isCompleted"),
+	inflection: function() {
+		var remaining = this.get("remaining");
+		return remaining === 1 ? "todo" : "todos";
+	}.property("remaining")
 });
